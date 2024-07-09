0ed3d18314401bd3bc11b92e55cf8ba346ff4deb
da9dac <da9dac@gmail.com>
2024-07-09 10:49:52 +0900

    승자 및 무승부 결과 표시

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -2,7 +2,8 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
-
+import { WINNING_COMBINATIONS } from "./winningcombinations";
+import GameOver from "./components/GameOver";
 
 function deriveActivePlayer(gameTurns) {
   let curPlayer = 'X';
@@ -12,10 +13,39 @@ function deriveActivePlayer(gameTurns) {
   return curPlayer;
 }
 
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null]
+];
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
 
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+      const { square, player } = turn;
+      const { row, col } = square;
+
+      gameBoard[row][col] = player;
+  }
+
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const first = gameBoard[combination[0].row][combination[0].column];
+    const second = gameBoard[combination[1].row][combination[1].column];
+    const third = gameBoard[combination[2].row][combination[2].column];
+
+    if (first && first === second && first === third) {
+      winner = first;
+    }
+  }
+
+  const hasDraw = gameTurns.length === 9 && !winner;
 
   function handleSelectSquare(rowIdx, colIdx) {
     setGameTurns(
@@ -39,7 +69,8 @@ function App() {
           <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
           <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        {(winner || hasDraw ) && <GameOver winner={winner} />}
+        <GameBoard onSelectSquare={handleSelectSquare} gameBoard={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
